0e5de9f95a1df69954a4bf7c8ddfd295522a735a
kenyonz <237453156@qq.com>
2018-11-23 14:06:58 +0800

    删除中间多余文件夹

--- 无故障_使用最优个体生成填写excel用中间文件/Random.m
+++
@@ -1,6 +0,0 @@
-%% Ëæ»úÑ¡Ôñº¯Êý
-function target = Random(active_cnc)
-    active_num = length(active_cnc);
-    target = mod(round(rand(1) * 1000), active_num) + 1;
-    target = active_cnc(target);
-end
